237941c7d69c4dc85912e321dcc6d9f007a634b2
Thais Cruz <thais@nic.br>
2020-04-14 10:52:01 -0300

    Js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+ var tabsFn = function () {
+
+            function init() {
+                setHeight();
+            }
+
+            function setHeight() {
+                var tabPane = $('.tab-pane'),
+                    tabsHeight = $('.nav-tabs').height();
+
+                tabPane.css({
+                    height: tabsHeight
+                });
+
+            
+
+
+            }
+
+            $(init);
+        }();
