4b30bcda9e9312698752f43f4439e97aec2f26d4
Thais Cruz <thais@nic.br>
2020-05-01 03:45:22 -0300

    Comentando Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 //Tab altura
-var tabsFn = function () {
-  function init() {
-    setHeight();
-  }
+// var tabsFn = function () {
+//   function init() {
+//     setHeight();
+//   }
 
-  function setHeight() {
-    var tabPane = $('.tab-pane'),
-      tabsHeight = $('.nav-tabs').height();
+//   function setHeight() {
+//     var tabPane = $('.tab-pane'),
+//       tabsHeight = $('.nav-tabs').height();
     
-      tabPane.css({
-        height: tabsHeight
-      });
+//       tabPane.css({
+//         height: tabsHeight
+//       });
 
     
-  }
-  $(init);
-}();
+//   }
+//   $(init);
+// }();
 
 // Active das tr das tabelas
 $(document).on('click', 'tbody tr', function (e) {
